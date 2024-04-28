934a1c14314dc42a5f1001edb84eb29da58a7dab
Bengie <bvillesco@gmail.com>
2024-04-28 17:43:29 +0800

    Lesson 4: Added Problem 5.py

---
+++ Lesson 4 - Python List/Problem 5.py
@@ -0,0 +1,22 @@
+list = [8, 9, 10]
+
+# Insert 17 at index 1.
+list.insert(1, 17)
+
+# Extend the list with these elements.
+list.extend([4, 5, 6])
+
+# Remove the 8 using pop at index 0.
+list.pop(0)
+
+# Sort the list
+list.sort()
+
+# Duplicate the elements in the list.
+list *= 2
+
+# Insert 25 at index 3.
+list.insert(3, 25)
+
+# Print the list.
+print(list)
