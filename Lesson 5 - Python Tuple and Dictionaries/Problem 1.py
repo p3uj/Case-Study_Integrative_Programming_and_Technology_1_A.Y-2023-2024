2c2bf52c7cbef99723645c7df544cf3b5b1860bf
Bengie <bvillesco@gmail.com>
2024-04-28 17:11:40 +0800

    Lesson 5: Added Problem 1.py

---
+++ Lesson 5 - Python Tuple and Dictionaries/Problem 1.py
@@ -0,0 +1,8 @@
+input_list = [1, 2, 3]  # Initial element of the list.
+
+for item in input_list: # Loop through each of the elements.
+    input_list[input_list.index(item)] = (item, item*item)  # Update the element.
+
+# Convert the input_list into tuple to create tuple with the elements of number and its square of that number.
+tuple_elements = tuple(input_list)
+print(tuple_elements)
