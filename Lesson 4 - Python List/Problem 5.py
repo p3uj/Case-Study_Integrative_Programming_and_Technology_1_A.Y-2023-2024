f9303b840bc9651e9cbf7edd0094683fca6d241f
Bengie <bvillesco@gmail.com>
2024-04-28 17:54:10 +0800

    Lesson 4: Added Problem 5.py

---
+++ Lesson 4 - Python List/Problem 5.py
@@ -0,0 +1,21 @@
+# Start with the initial list
+my_list = [8, 9, 10]
+
+# a.
+my_list[1] = 17
+
+# b.
+my_list.extend([4, 5, 6])
+
+# c.
+my_list.pop(0)
+
+# d.
+my_list.sort()
+
+# e.
+my_list *= 2
+
+# f.
+my_list.insert(3, 25)
+print(my_list)
