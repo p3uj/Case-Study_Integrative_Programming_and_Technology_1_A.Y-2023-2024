95d97aba99efa442106065e9dd04eaf5ee834c94
Bengie <bvillesco@gmail.com>
2024-04-28 17:02:43 +0800

    Added Problem_2.py

---
+++ Lesson 4 - Python List/Problem_2.py
@@ -0,0 +1,19 @@
+def list_of_items():
+    items = []
+
+    # Add the first four items to the list called items using append. (one per item).
+    items.append(67)
+    items.append(62.9)
+    items.append("hi")
+    items.append(False)
+
+    # Add the last four items to the list called items using concatenate.
+    items += [8]
+    items += [67]
+    items += ['apple']
+    items += [6.5]
+
+    return items
+
+# Function call to list_of_items function then display the return list of items.
+print(list_of_items(), end='')  # The end='' is used to specify that there is no newline.
