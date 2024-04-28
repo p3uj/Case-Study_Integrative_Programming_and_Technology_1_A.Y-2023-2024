e15d563546ef649691de2e80c20ed9d80fdaeb0a
Bengie <bvillesco@gmail.com>
2024-04-28 17:53:42 +0800

    Lesson 4: Added Problem 2.py

---
+++ Lesson 4 - Python List/Problem 2.py
@@ -0,0 +1,8 @@
+# Create an empty list and add items using append method and concatenation
+my_list = []
+my_list.append(67)
+my_list.append(62.9)
+my_list.append("hi")
+my_list.append(False)
+my_list = my_list + [8, 67, 'apple', 6.5]
+print(my_list)
