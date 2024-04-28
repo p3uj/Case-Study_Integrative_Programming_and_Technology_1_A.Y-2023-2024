e0818912deda8aa782bbc637deb32294a9ca2379
Bengie <bvillesco@gmail.com>
2024-04-28 17:24:14 +0800

    Created new branch called dev/macas-edzra and added Problem 1.py

---
+++ Lesson 4 - Python List/Problem 1.py
@@ -0,0 +1,31 @@
+"""
+Problem 1: Define the variables x and y as lists of numbers
+- x=[1, 2, 3, 4, 5]
+- y=[11, 12, 13, 14, 15]
+- z=[21, 22, 23, 24, 25]
+
+a. What is the value of 3*x?
+answer: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+b. What is the value of x+y?
+answer: [1, 2, 3, 4, 5, 11, 12, 13, 14, 15]
+c. What is the value of x-y?
+answer: ERROR.
+d. What is the value of x[1]?
+answer: 2
+e. What is the value of x[0]?
+answer: 1
+f. What is the value of x[-1]?
+answer: 5
+g. What is the value of x[:]?
+answer: [1, 2, 3, 4, 5]
+h. What is the value of x[2:4]?
+answer: [3, 4]
+i. What is the value of x[1:4:2]?
+answer: [2, 4]
+j. What is the value of x[:2]?
+answer: [1, 2]
+k. What is the value of x[::2]?
+answer: [1, 3, 5]
+l. What is the result of the following two expressions?
+answer: [1, 2, 3, 8, 5]
+"""
