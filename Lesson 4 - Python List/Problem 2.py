5d2fb1bf7073c221aa5ec4ab9adea345d30d0913
Bengie <bvillesco@gmail.com>
2024-04-28 17:42:51 +0800

    Lesson 4: Added Problem 2.py

---
+++ Lesson 4 - Python List/Problem 2.py
@@ -0,0 +1,16 @@
+my_list = []
+
+# Append method to add one per item.
+my_list.append(67)
+my_list.append(62.9)
+my_list.append("hi")
+my_list.append(False)
+
+# Concatenation
+my_list = my_list + [8]
+my_list = my_list + [67]
+my_list = my_list + ['apple']
+my_list = my_list + [6.5]
+
+# Print
+print(my_list)
