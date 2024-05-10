55692a5e63eb54c498e787dc7ff229548ee03646
Bengie <bvillesco@gmail.com>
2024-05-10 22:27:27 +0800

    Modified Lesson 5/Problem 3.py: Added instruction using comment. (Bentulan's solution)

--- Lesson 5 - Python Tuple and Dictionaries/Problem 3.py
+++ Lesson 5 - Python Tuple and Dictionaries/Problem 3.py
@@ -1,3 +1,14 @@
+"""
+Instruction: Here is a dictionary of the days in the months of the year:
+- days = {'January':31, 'February':28, 'March':31, 'April':30, 'May':31, 'June':30, 'July':31, 'August':31, 
+'September':30, 'October':31, 'November':30, 'December':31}
+
+a. Ask the user to enter a month name and use the dictionary to tell them how many days are in the month.
+b. Print out all of the keys in alphabetical order.
+c. Print out all of the months with 31 days.
+d. Print out the (key-value) pairs sorted by the number of days in each month
+"""
+
 days = {'January': 31, 'February': 28, 'March': 31, 'April': 30, 'May': 31,
         'June': 30, 'July': 31, 'August': 31, 'September': 30, 'October': 31,
         'November': 30, 'December': 31}
