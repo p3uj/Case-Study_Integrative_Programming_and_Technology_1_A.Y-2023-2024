795bbfd29d7a1f06b9f904b99ea4debefa0a0960
Edzra Macas <EdzraM7@gmail.com>
2024-05-10 18:14:04 +0800

    L4 #6

--- Lesson 4 - Python List/Problem 6.py
+++ Lesson 4 - Python List/Problem 6.py
@@ -1,9 +1,9 @@
-factors = []
-num = int(input("Enter an integer: "))
+factors = []#intializes an empty list
+num = int(input("Enter an integer: "))#accepts an integer user input to find its factor
 
-for x in range(1, num + 1):
+for x in range(1, num + 1):#iterates from 1 to num to check if x is a factor of num
     if num % x == 0:
         factors.append(x)
 
 print(f"Factors of {num}: ", end=" ")
-print(*factors, sep=',')
+print(*factors, sep=',')#prints the factors of num separated by comma
