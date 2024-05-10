6e7ac5af116ec3726eee490980111f18547217f2
Bengie <bvillesco@gmail.com>
2024-05-10 22:11:18 +0800

    Lesson 5: Added Problem 2.py

---
+++ Lesson 5 - Python Tuple and Dictionaries/Problem 2.py
@@ -0,0 +1,18 @@
+print("Enter numerical values for tuple (separate by space):")
+while True:
+    try:
+        tuple1 = tuple([int(x) for x in input().split()]) # Map to map int() to each element
+        break
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+
+try:
+    k = int(input("Enter an integer: "))
+
+
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+   
+tuple2 = [x for x in tuple1 if x % k == 0]
+print("Elements divisible by", k, "are:", tuple2)
