c57564172db76f4f3476475e22daca1803fd5967
JohannaSillano <163633918+JohannaSillano@users.noreply.github.com>
2024-05-10 19:11:06 +0800

    Update Problem 1.py

--- Lesson 5 - Python Tuple and Dictionaries/Problem 1.py
+++ Lesson 5 - Python Tuple and Dictionaries/Problem 1.py
@@ -1,5 +1,8 @@
+# Initialize the list with the given numbers.
 tup = (1, 2, 3)
 
-# Create a list; Iterate over each element.
+# Create a list using a for loop to iterate over each element of the tuple, generating a tuple of each element and its square.
 output = [(num, num **2) for num in tup]
+
+# Print the content of output.
 print(output)
