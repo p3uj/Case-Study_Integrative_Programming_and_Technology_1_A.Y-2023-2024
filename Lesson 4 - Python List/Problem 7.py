75d6d58b94d5f3974ef9076967e2bf53570b4f67
Bengie <bvillesco@gmail.com>
2024-05-10 21:39:33 +0800

    Added Problem 7.py (Villesco's solution)

---
+++ Lesson 4 - Python List/Problem 7.py
@@ -0,0 +1,11 @@
+"""
+Instruction: Write a program that removes any repeated items from a list so that each item appears at most once.
+- For instance, the list [1,1,2,3,4,3,0,0] would become [1,2,3,4,0].
+"""
+
+elements_list = [1, 1, 2, 3, 4, 3, 0, 0]    # Initial elements of the list.
+for i in elements_list: # Loop until it reach the last elements in the list.
+    if elements_list.count(i) > 1:  # Check and execute if the occurrences of the element is more than 1.
+        second_index = elements_list.index(i, elements_list.index(i) + 1)   # Find the second index of the element.
+        elements_list.pop(second_index) # Remove the element in the second index using pop method.
+print(elements_list)    # Print the updated elements in the list.
