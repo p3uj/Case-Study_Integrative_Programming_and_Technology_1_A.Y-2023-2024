817708e42d8298ef488f727d08d8ce77abc281b5
Bengie <bvillesco@gmail.com>
2024-05-10 21:06:04 +0800

    Added Problem 6.py (Macas solution)

---
+++ Lesson 4 - Python List/Problem 6.py
@@ -0,0 +1,11 @@
+# Instruction: Write a program that asks the user for an integer and creates a list that consists of the factors of that integer.
+
+factors = []#intializes an empty list
+num = int(input("Enter an integer: "))#accepts an integer user input to find its factor
+
+for x in range(1, num + 1):#iterates from 1 to num to check if x is a factor of num
+    if num % x == 0:
+        factors.append(x)
+
+print(f"Factors of {num}: ", end=" ")
+print(*factors, sep=',')#prints the factors of num separated by comma
