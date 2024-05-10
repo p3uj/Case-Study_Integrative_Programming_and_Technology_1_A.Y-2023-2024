9bf885ddccf8b780ed1bd51e388b9ec1636a808d
JohannaSillano <163633918+JohannaSillano@users.noreply.github.com>
2024-05-10 19:10:01 +0800

    Update Problem 2.py

--- Lesson 4 - Python List/Problem 2.py
+++ Lesson 4 - Python List/Problem 2.py
@@ -1,16 +1,18 @@
+# Initialize an empty list.
 my_list = []
 
-# Append method to add one per item.
+# append() method to add one per item.
 my_list.append(67)
 my_list.append(62.9)
 my_list.append("hi")
 my_list.append(False)
 
-# Concatenation
+# Concatenate the list with elements using "+" operator.
 my_list = my_list + [8]
 my_list = my_list + [67]
 my_list = my_list + ['apple']
 my_list = my_list + [6.5]
 
-# Print
+# Print the content of my_list.
 print(my_list)
+
