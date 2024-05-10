27e7ee4972b842f849fe6a9e4601d5fa4aa0526c
Edzra Macas <EdzraM7@gmail.com>
2024-05-10 17:59:17 +0800

    L4 #5

--- Lesson 4 - Python List/Problem 5.py
+++ Lesson 4 - Python List/Problem 5.py
@@ -1,12 +1,12 @@
-List = [8, 9, 10]
+List = [8, 9, 10]#declares a list with its elements 8, 9, and 10
 
-print("Initial list: ", List)
+print("Initial list: ", List)#prints the initial list
 
-List[1] = 17
-List += [4, 5, 6]
-List.pop(0)
-List.sort()
-List *= 2
-List.insert(3, 25)
+List[1] = 17#sets the second element to 17
+List += [4, 5, 6]#appends 4,5,6 to the list
+List.pop(0)#removes the first element of the list
+List.sort()#sorts the list in ascending order
+List *= 2#doubles the list
+List.insert(3, 25)#inserts 35 to the 4th position
 
-print("Final list: ", List)
+print("Final list: ", List)#prints the final list
