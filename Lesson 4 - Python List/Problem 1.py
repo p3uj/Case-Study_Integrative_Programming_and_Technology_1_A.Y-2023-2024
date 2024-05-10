60e3bed029c5b0018bff2254b57ed29bbc3f8576
Mikasa179 <144320467+Mikasa179@users.noreply.github.com>
2024-05-11 01:31:41 +0800

    Update Problem 1.py

--- Lesson 4 - Python List/Problem 1.py
+++ Lesson 4 - Python List/Problem 1.py
@@ -21,22 +21,22 @@ answer: 1
 #x[-1] accesses the last element of the list x
 f. What is the value of x[-1]?
 answer: 5
-#a slice that selects all the elements of the list x
+#x[:] slices the list x from the first index to the last index
 g. What is the value of x[:]?
 answer: [1, 2, 3, 4, 5]
-#slices the list from index 2 to, but not including, index 4
+#x[2:4] slices the list from index 2 to, but not including, index 4
 h. What is the value of x[2:4]?
 answer: [3, 4]
-#slices the list from index 1, but not including, index 4 with a step size of 2
+#x[1:4:2] slices the list from index 1, but not including, index 4 with a step size of 2
 i. What is the value of x[1:4:2]?
 answer: [2, 4]
-#slices the list from the first index to, but not including, index 4
+#x[:2] slices the list from the first index to, but not including, index 2
 j. What is the value of x[:2]?
 answer: [1, 2]
-#a slice that selects all the elements of the list with a step of 2
+#x[::2] slices the list x from the first index to the last index with a step of 2
 k. What is the value of x[::2]?
 answer: [1, 3, 5]
-#x[3] sets the element in the 4th position to 8
+#x[3] = 8 sets the element in the 4th position to 8 and print x prints the list x
 l. What is the result of the following two expressions?
 x[3]=8 
 print x
